0ce97635a17deb60655bb22fc9ccb98f6b175559
AndyGH86 <141663667+AndyGH86@users.noreply.github.com>
2023-11-15 09:18:15 +0100

    cambios

--- src/App.js
+++ src/App.js
@@ -1,38 +1,73 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TiDeleteOutline } from "react-icons/ti";
 import './App.css';
+
 
 function App() {
   const [inputValue, setInputValue] = useState('');
   const [list, setList] = useState([]);
 
-    return (<>  
-  <div className='todolist-container'>
+  const obtenerInfoServer = async () => {
+ 
+      const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/Anderson');
+      const data = await response.json();
+      setList(data);
 
-    <h1>Todos</h1>
-    <ul>
-      <li><input
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        onKeyPress={(e) => {
-          if (e.key === "Enter") {
-            setList(list.concat(inputValue));
-            setInputValue("");
-          }
-        }}
-        placeholder="What do you need to do?"
-      /></li>
-       {list.map((l, index) => (
-      <li>
-        {l}{""} <button onClick={() => {
-          setList( list.filter((a, currentIndex) => index !== currentIndex))
-        }}><TiDeleteOutline/></button>
-      </li>))}
-      <li> {list.length} "item left"</li>
-    </ul>
-      
-  </div>
-    
+  };
+
+  useEffect(() => {
+    obtenerInfoServer();
+  }, []);
+
+  return (
+    <>
+      <div className='todolist-container'>
+        <h1>Todos</h1>
+        <ul>
+          <li>
+            <input
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              onKeyPress={(e) => {
+                if (e.key === 'Enter') {
+                  const newTodo = { label: inputValue, done: false };
+                  const newList = [ newTodo, ...list];
+                  fetch('https://playground.4geeks.com/apis/fake/todos/user/Anderson', {
+                    method: 'PUT',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(newList),
+                    
+                  })
+                  .then(() => {
+                    setList(newList);
+                    setInputValue('');
+                    console.log(newList)
+                  })
+                  
+                  
+                }
+              }}
+              placeholder='What do you need to do?'
+            />
+          
+          </li>
+          {list.map((task, index) => (
+            <li key={index}>
+              {task.label}{' '}
+              <button onClick={() => {
+                  const updatedList = list.filter((_, currentIndex) => index !== currentIndex);
+                  fetch('https://playground.4geeks.com/apis/fake/todos/user/Anderson', {
+                    method: 'PUT',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(updatedList),
+                  });
+                  obtenerInfoServer(); 
+              }}><TiDeleteOutline /></button>
+            </li>
+          ))}
+          <li>{list.length} items left</li>
+        </ul>
+      </div>
     </>
   );
 }
